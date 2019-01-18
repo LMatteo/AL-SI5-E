d1c30ba7d729ba2f311a0cff745399354475c8d5
Lucas Matteo <lucas.matteo@etu.unice.fr>
2019-01-18 12:06:52 +0100

    contract instance test

--- blablamoveService/typescript/main/src/components/contract-instance/ContractInstance.ts
+++ blablamoveService/typescript/main/src/components/contract-instance/ContractInstance.ts
@@ -5,9 +5,9 @@ import {Notify} from "../agency-notifier/Notify";
 import {Subscribe} from "../../entity/Subscribe";
 import {Customer} from "../../entity/customer/Customer";
 import {Contract} from "../../entity/contract/Contract";
-import {AgencyNotifier} from "../agency-notifier/AgengyNotifier";
 import {inject, injectable} from "inversify";
 import COMPONENT_IDENTIFIER from "../InjectionIdentifier";
+import {NoSuchSubscription} from "../../error/NoSuchSubscription";
 
 
 @injectable()
@@ -38,8 +38,13 @@ export class ContractInstance implements GetSubscription, Subscription{
         }
         return res;
     }
-    getSubscriptionById(id: string): Subscribe {
-        throw new Error("Method not implemented.");
+    getSubscriptionById(id: number): Subscribe {
+        for(let sub of this.store.get()){
+            if(sub.$id ===id){
+                return sub
+            }
+        }
+        throw new NoSuchSubscription();
     }
 
     subscribeToContract(customer: Customer , contract: Contract): Subscribe {
---
+++ blablamoveService/typescript/test/src/local/contract-instance/ContractInstance.spec.ts
@@ -0,0 +1,42 @@
+import 'reflect-metadata'
+import {GetSubscription} from "../../../../main/src/components/contract-instance/GetSubscription";
+import {inject} from "inversify";
+import COMPONENT_IDENTIFIER from "../../../../main/src/components/InjectionIdentifier";
+import {Subscription} from "../../../../main/src/components/contract-instance/Subscription";
+import {SubscribeStore} from "../../../../main/src/entityManager/local/SubscribeStore";
+import {Customer} from "../../../../main/src/entity/customer/Customer";
+import {Contract} from "../../../../main/src/entity/contract/Contract";
+import {Type} from "../../../../main/src/entity/Type";
+import {Contact} from "../../../../main/src/entity/contact/Contact";
+import * as assert from "assert";
+import container from "../../../../main/src/components/InjectionConfig";
+import {NoSuchSubscription} from "../../../../main/src/error/NoSuchSubscription";
+
+
+
+describe('contract instance test', function () {
+
+    let getSubs : GetSubscription;
+    let subs : Subscription;
+
+
+    beforeEach(() => {
+       new SubscribeStore().clear();
+       getSubs = container.get(COMPONENT_IDENTIFIER.GetSubscription);
+       subs = container.get(COMPONENT_IDENTIFIER.Subscription);
+    });
+
+    it('should had and retrive subscription', function () {
+        let custo = new Customer();
+        let contract = new Contract("test",Type.fragile,new Contact("test"));
+        let subscription = subs.subscribeToContract(custo,contract);
+
+        assert.strictEqual(1,getSubs.getSubscriptionByCustomer(custo).length);
+        assert.deepStrictEqual(subscription,getSubs.getSubscriptionByCustomer(custo)[0]);
+        assert.deepStrictEqual(subscription,getSubs.getSubscriptionById(subscription.$id))
+    });
+
+    it('should throw an error', function () {
+        assert.throws(() => getSubs.getSubscriptionById(900000),NoSuchSubscription)
+    });
+});
