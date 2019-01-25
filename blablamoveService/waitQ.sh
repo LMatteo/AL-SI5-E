e0b858932fe1cd3f4224613ef389279e0a1ee527
Shiyang HUANG <shiyang.huang@etu.unice.fr>
2019-01-25 15:02:00 +0100

    fix waitQ

--- blablamoveService/waitQ.sh
+++ blablamoveService/waitQ.sh
@@ -4,7 +4,7 @@ qIP=`docker inspect -f '{{range .NetworkSettings.Networks}}{{.IPAddress}}{{end}}
 
 res=curl -s -o /dev/null -I -w "%{http_code}" -u guest:guest http://$qIP:15672/api/overview
 
-while [ ! $res -eq 200 ]; do
+while [ ! $res -eq '200' ]; do
     echo 'Waiting to queue to be up ... (code='$res')'
     sleep 2
     res=curl -s -o /dev/null -I -w "%{http_code}" -u guest:guest http://$qIP:15672/api/overview
