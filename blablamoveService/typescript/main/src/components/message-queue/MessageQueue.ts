dd7cca162f591fb1a6f65a5c3b537caccc4b2686
Francois MELKONIAN <francois.melkonian@etu.unice.fr>
2019-01-25 09:34:18 +0100

    work around db connection duplications, add entity policy

--- blablamoveService/typescript/main/src/components/message-queue/MessageQueue.ts
+++ blablamoveService/typescript/main/src/components/message-queue/MessageQueue.ts
@@ -31,7 +31,7 @@ export class MessageQueue{
             var queue = MessageQueue.connection.declareQueue(MessageQueue.VALIDATION_QUEUE);
             queue.bind(MessageQueue.exchangeValidation);
             queue.activateConsumer((message) => {
-                console.log("consume validate" + message.getContent());
+                console.log("exchangeValidation")
                 let travelMarshalled = JSON.parse(message.getContent());
                 let travel: Travel = travelMarshalled;
                 travel.$customer = travelMarshalled.customer || new Customer();
