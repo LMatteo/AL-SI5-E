5fb7a806f293588159a12406020c6fd043440e48
Francois MELKONIAN <francois.melkonian@etu.unice.fr>
2019-02-15 10:24:54 +0100

    insuranceComparator return cheapest contract

--- insuranceComparator/typescript/main/src/Main.ts
+++ insuranceComparator/typescript/main/src/Main.ts
@@ -2,13 +2,10 @@ import express = require("express");
 import bodyParser = require("body-parser");
 import {Queue} from "./queue/Queue";
 import {Contract} from "./entity/Contract";
+import {Item} from "./entity/Item";
 
 const app: express.Express = express();
 
-app.use(function (req: express.Request, res: express.Response, next: express.NextFunction) {
-    console.log("new request");
-    next();
-});
 app.use(bodyParser.json());
 
 app.use(function (req: express.Request, res: express.Response, next: express.NextFunction) {
@@ -18,12 +15,39 @@ app.use(function (req: express.Request, res: express.Response, next: express.Nex
 let contracts: Contract[] = [];
 let host: string = process.env.blablamove_queue || "amqp://localhost:5672";
 
-app.get('/searchContract', function (req, res) {
-    if(contracts.length < 1){
+app.post('/searchContract', function (req, res) {
+    if(req.body.items === undefined || req.body.items.length < 1){
+        res.statusCode = 503;
+        res.send("'items' expected");
+        return;
+
+    }
+    let bestContract : Contract= undefined;
+    for (let i = 0; i < req.body.items.length; i++) {
+        let item = req.body.items[i];
+        let item1 = new Item(item.name, item.type);
+        for (let i = 0; i < contracts.length; i++) {
+            if(contracts[i].type ==item1.type){
+                if(bestContract === undefined)
+                    bestContract = contracts[i];
+                else if(bestContract.totalprice > contracts[i].totalprice){
+                    bestContract = contracts[i];
+                }
+            }
+        }
+        if(bestContract === undefined){
+            res.statusCode = 509;
+            res.send("No contract matching");
+            return;
+
+        }else{
+            res.send(bestContract);
+        }
+    }
+    if(contracts.length < 1) {
         res.statusCode = 404;
         res.send("No contracts found");
-    }else{
-        res.send(contracts[contracts.length - 1]);
+        return;
     }
 });
 let queue: Queue = new Queue(host);
--- insuranceComparator/typescript/main/src/entity/Contract.ts
+++ insuranceComparator/typescript/main/src/entity/Contract.ts
@@ -4,7 +4,7 @@ export class Contract {
     public type: string;
     public policies: Policy[];
     public id: number;
-
+    public totalprice: number;
     constructor(id: number, type: string, policies: Policy[]) {
         this.id = id;
         this.type = type;
