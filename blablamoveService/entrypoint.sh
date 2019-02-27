2cf457731cd63f5e186bc802786d9a404f0cdf0d
Shiyang HUANG <shiyang.huang@etu.unice.fr>
2019-02-27 23:49:32 +0100

    fix launch dependency

---
+++ blablamoveService/entrypoint.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+mysql -h$dbName -uuser -puser > /dev/null 2> /dev/null
+
+res=$?
+while [ ! $res -eq 0 ]; do
+    echo 'Waiting to database to be up ... (code='$res')'
+    sleep 2
+    mysql -h$dbName -uuser -puser > /dev/null 2> /dev/null
+    res=$?
+done
+
+npm start
